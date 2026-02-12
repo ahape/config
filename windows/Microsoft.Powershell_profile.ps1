71735e765e751c7adceaef26962652706d36c633
Alan Hape <ahape@brightmetrics.com>
2026-02-12 12:40:20 -0800

    Update profile custom ipmo calls

--- windows/Microsoft.Powershell_profile.ps1
+++ windows/Microsoft.Powershell_profile.ps1
@@ -4,11 +4,14 @@ $global:Repo = 'C:\src\projects'
 # Aliases
 Set-Alias -Name vi -Value vim -ErrorAction Ignore
 
-# Custom modules
-if ($PSVersionTable.PSVersion.Major -ge 7) {
-  Start-ThreadJob { Import-CustomModules } | Out-Null
-} else {
-  Import-CustomModules
+# Lazy load custom modules. Requires the first invocation to be without auto-completed anything
+$ExecutionContext.InvokeCommand.PreCommandLookupAction = {
+  param($command)
+  if ($command -in 'Invoke-LLM', 'Ask-LLM', 'Show-Markdown', 'Render-Markdown') {
+    $ExecutionContext.InvokeCommand.PreCommandLookupAction = $null
+    Import-Module (Join-Path $HOME 'source\repos\llmchat\Invoke-LLM.psm1') -ErrorAction SilentlyContinue
+    Import-Module (Join-Path $HOME 'source\repos\markterm\Show-Markdown.psm1') -ErrorAction SilentlyContinue
+  }
 }
 
 # Functions
@@ -22,13 +25,4 @@ function prompt {
   }
 
   "PS $path> "
-}
-
-function Import-CustomModules {
-  @(
-    Join-Path $HOME 'source\repos\llmchat\Invoke-LLM.psm1'
-    Join-Path $HOME 'source\repos\markterm\Show-Markdown.psm1'
-  ) `
-  | ? { Test-Path $_ } `
-  | % { Import-Module $_ -ErrorAction SilentlyContinue }
 }
