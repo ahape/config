429cf94d53f394ffbf85ca0d3e929b83fef2d53a
Alan Hape <ahape@brightmetrics.com>
2026-03-12 10:58:35 -0700

    Fix Git-Show to use smarter browser launcher

--- windows/Scripts/Git-Show.ps1
+++ windows/Scripts/Git-Show.ps1
@@ -8,4 +8,4 @@ param(
 $remoteUrl=(git config --get remote.origin.url).Replace(".git", "/commit")
 
 # Assuming the default application for opening links is set up
-explorer.exe "$remoteUrl/$($CommitHash.Trim())?w=1"
+Start-Process "$remoteUrl/$($CommitHash.Trim())?w=1"
